78a8655facd3f3d9fae6d2e4a8a48ba84802d79c
Denise "Eliza" Cherry-Russell <108440338+ElizaMarie887@users.noreply.github.com>
2022-09-12 13:24:23 -0500

    Add Stretch goal

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ console.log ("HH   HH   eee  lll lll  oooo  ww     ww   oooo  rr rr  lll     dd 
 console.log ("HHHHHHH ee   e lll lll oo  oo ww     ww  oo  oo rrr  r lll  ddddd !!!") 
 console.log ("HH   HH eeeee  lll lll oo  oo  ww ww ww  oo  oo rr     lll dd  dd !!!")
 console.log ("HH   HH  eeeee lll lll  oooo     ww ww    oooo  rr     lll  ddddd !!!")
+
+
+console.log ("EEEEE lll ii")
+console.log ("EE    lll     zzzz  aaaa")
+console.log ("EEEEE lll ii    zz  a   a")
+console.log ("EE    lll ii  z     a   a ")
+console.log ("EEEEE lll ii zzzz   aaaa a")
